90ce3a6433a708edb2d6d14b4433834fe558924d
tveritina <tveritinanastya@gmail.com>
2020-05-03 17:15:19 +0500

    Отредактирован  скрипт для модальных окон

--- source/js/script.js
+++ source/js/script.js
@@ -2,20 +2,37 @@ var feedbackForm = document.querySelector(".review-form");
 var popupsError = document.querySelector("#popups-error");
 var popupsSent = document.querySelector("#popups-sent");
 var popupsButtonSend = document.querySelector(".popups__button-send");
+var popupsButtonOk = document.querySelector(".popups__button-ok");
 
 if (feedbackForm) {
   var submitButton = feedbackForm.querySelector(".button");
-};
-
-if (submitButton) {
-  submitButton.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popupsSent.classList.add("popups-show");
-  });
+  var firstName = document.querySelector("#firstname");
+  var lastName = document.querySelector("#lastname");
+  var contactsPhone = document.querySelector("#contacts-phone");
+  var contactsEmail = document.querySelector("#contacts-email");
 };
 
 if (popupsButtonSend) {
   popupsButtonSend.addEventListener("click", function () {
-      popupsSent.classList.remove("popups-show");
+    popupsSent.classList.remove("popups-show");
+  });
+};
+
+if (popupsButtonOk) {
+  popupsButtonOk.addEventListener("click", function () {
+    popupsError.classList.remove("popups-show");
+  });
+};
+
+if (feedbackForm) {
+  feedbackForm.addEventListener("submit", function (evt) {
+    if (!firstName.value || !lastName.value || !contactsPhone.value || !contactsEmail.value) {
+      evt.preventDefault();
+      popupsError.classList.add("popups-show");
+    }
+    else {
+      evt.preventDefault();
+      popupsSent.classList.add("popups-show");
+    }
   });
 };
