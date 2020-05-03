dfe448769061f09c2da1ed390d8b443ad5bdf93e
tveritina <tveritinanastya@gmail.com>
2020-05-03 17:31:04 +0500

    Добавлены обработчики модальных окон

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,6 @@ var popupsButtonSend = document.querySelector(".popups__button-send");
 var popupsButtonOk = document.querySelector(".popups__button-ok");
 
 if (feedbackForm) {
-  var submitButton = feedbackForm.querySelector(".button");
   var firstName = document.querySelector("#firstname");
   var lastName = document.querySelector("#lastname");
   var contactsPhone = document.querySelector("#contacts-phone");
@@ -28,11 +27,26 @@ if (feedbackForm) {
   feedbackForm.addEventListener("submit", function (evt) {
     if (!firstName.value || !lastName.value || !contactsPhone.value || !contactsEmail.value) {
       evt.preventDefault();
+
+      if (!firstName.value) {
+        firstName.classList.add("review-form-error");
+      }
+      if (!lastName.value) {
+        lastName.classList.add("review-form-error");
+      }
+      if (!contactsPhone.value) {
+        contactsPhone.classList.add("review-form-error");
+      }
+      if (!contactsEmail.value) {
+        contactsEmail.classList.add("review-form-error");
+      }
+
       popupsError.classList.add("popups-show");
     }
     else {
       evt.preventDefault();
       popupsSent.classList.add("popups-show");
+      feedbackForm.reset();
     }
   });
 };
